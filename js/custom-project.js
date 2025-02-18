c0f10171141f386af7d60af1c832b67290ea41d8
jmsanchez24 <161487164+jmsanchez24@users.noreply.github.com>
2025-02-18 14:08:17 -0600

    move to correct spot

--- js/custom-project.js
+++ js/custom-project.js
@@ -19,7 +19,8 @@
   
       $('#FormUpdate').load('flows/prod-custom-flow.html', function() {
         $(".prod-flow .tab").first().addClass("active");
-
+        console.log("test");
+        console.log(MenuOptionText);
         showNextPrevBtn();
       });
   
@@ -29,8 +30,6 @@
   
       $('#FormUpdate').load('flows/support-custom-flow.html', function() {
         $(".support-flow .tab").first().addClass("active");
-        console.log("test");
-        console.log(MenuOptionText);
         showNextPrevBtn();
       });
   
