fad5245d3a7e252f7aa0db0406d737caa9ac86fb
jmsanchez24 <161487164+jmsanchez24@users.noreply.github.com>
2025-04-07 13:20:39 -0500

    optimize code

--- js/custom-project.js
+++ js/custom-project.js
@@ -1,301 +1,220 @@
 (function($) {
-  var MenuOptionVal;
-  var MenuOptionText;
-  
+  let MenuOptionVal;
+  let MenuOptionText;
 
-  function showNextPrevBtn(){
+  function showNextPrevBtn() {
+    const $active = $(".active");
 
-    if(!$(".active").hasClass("last-tab")){
-      $('#nextBtn').show();
-      $('#prevBtn').show();
-    }else{
+    if ($active.hasClass("last-tab")) {
       $('#nextBtn').hide();
-    }
-
-    if($(".active").hasClass("initial-tab")){
+    } else {
       $('#nextBtn').show();
-      $('#prevBtn').hide();
     }
-    
-  }
-  
-  function loadDeptFlow(currentTabOptionChecked){
 
-    if(currentTabOptionChecked == "is_prod"){
-  
-      $(".active").removeClass("active");
-  
-      $('#FormUpdate').load('flows/prod-custom-flow.html', function() {
+    $('#prevBtn').toggle(!$active.hasClass("initial-tab"));
+  }
+
+  function loadDeptFlow(selectedOption) {
+    $(".active").removeClass("active");
+
+    const flows = {
+      is_prod: 'flows/prod-custom-flow.html',
+      is_support: 'flows/support-custom-flow.html',
+      is_ms: 'flows/ms-custom-flow.html'
+    };
+
+    const flowPath = flows[selectedOption];
+    if (!flowPath) return;
+
+    $('#FormUpdate').load(flowPath, function() {
+      if (selectedOption === "is_prod") {
         $(".prod-flow .tab").first().addClass("active");
         $(".prod-customproject-name").text(MenuOptionText);
-        if(MenuOptionText == "Homepage Redevelopment"){
-          $(".confluence-prod-link").attr("href", "https://carscommerce.atlassian.net/wiki/spaces/PRODSQUAD/pages/2431911308/Custom+and+Redevelopments+Scope+Work+Production#%F0%9F%8E%AD-How-to%3A-Roles-and-Responsibilities");
-        }else{
-          $(".confluence-prod-link").attr("href", "https://carscommerce.atlassian.net/wiki/spaces/PRODSQUAD/pages/4599251433/Template+Swaps");
-        }
-        
-        showNextPrevBtn();
-      });
-  
-    }else if(currentTabOptionChecked == "is_support"){
-  
-      $(".active").removeClass("active");
-  
-      $('#FormUpdate').load('flows/support-custom-flow.html', function() {
-        if(MenuOptionVal == "homeTmpSwapSupport"){
+
+        const url = MenuOptionText === "Homepage Redevelopment"
+          ? "https://carscommerce.atlassian.net/wiki/spaces/PRODSQUAD/pages/2431911308/Custom+and+Redevelopments+Scope+Work+Production#%F0%9F%8E%AD-How-to%3A-Roles-and-Responsibilities"
+          : "https://carscommerce.atlassian.net/wiki/spaces/PRODSQUAD/pages/4599251433/Template+Swaps";
+
+        $(".confluence-prod-link").attr("href", url);
+      }
+
+      if (selectedOption === "is_support") {
+        if (MenuOptionVal === "homeTmpSwapSupport") {
           $(".support-flow .tab.sb-temp-swap").addClass("active");
           $(".sb-tmp-text").text(MenuOptionText);
-
-        }else{
+        } else {
           $(".support-flow .tab.support-number-sites").addClass("active");
-
         }
-        showNextPrevBtn();
-      });
-  
-    }else if(currentTabOptionChecked == "is_ms"){  
-  
-      $(".active").removeClass("active");
-  
-      $('#FormUpdate').load('flows/ms-custom-flow.html', function() {
+      }
+
+      if (selectedOption === "is_ms") {
         $(".ms-flow .tab").first().addClass("active");
-
         $(".ms-customproject-name").text(MenuOptionText);
-        
-        showNextPrevBtn();
-      });
-  
       }
-  
-   
-      
+
+      showNextPrevBtn();
+    });
   }
-  
+
   function nextslideLogic() {
-  
-    var nextTab = $(".active").next();
-    var currentFlow = $(".active").parent().attr('class');
-  
-    if(currentFlow == "inital-flow"){
+    const $active = $(".active");
+    const nextTab = $active.next();
+    const currentFlow = $active.parent().attr('class');
 
-      var currentTabOptionChecked = $('input[name=customProjMenu]:checked').val();
-      MenuOptionVal = $('input[name=customProjMenu]:checked').attr("id");
-      MenuOptionText = $('input[name=customProjMenu]:checked + label .custom-project-label').text();
+    if (currentFlow === "inital-flow") {
+      const $checked = $('input[name=customProjMenu]:checked');
+      MenuOptionVal = $checked.attr("id");
+      MenuOptionText = $checked.siblings("label").find(".custom-project-label").text();
 
-      if(MenuOptionVal == "customProjOther"){
+      if (MenuOptionVal === "customProjOther") {
         window.open("https://carscommerce.enterprise.slack.com/archives/C060TH869ME", "_blank");
-      }else{
-        loadDeptFlow(currentTabOptionChecked);
+      } else {
+        loadDeptFlow($checked.val());
       }
 
-      
-    }else if(currentFlow == "support-flow"){
+    } else if (currentFlow === "support-flow") {
+      SupportFlow(MenuOptionVal, MenuOptionText);
+      $active.removeClass("active");
+      nextTab.addClass("active");
+      showNextPrevBtn();
 
-        SupportFlow(MenuOptionVal, MenuOptionText);
-
-        $(".active").removeClass("active");
-        nextTab.addClass("active");
-        showNextPrevBtn();
- 
-    }else if(currentFlow == "ms-flow"){
-
-        MsFlow("next");
-
-      }
-  
+    } else if (currentFlow === "ms-flow") {
+      MsFlow("next");
+    }
   }
 
-  function SupportFlow(MenuOptionVal, MenuOptionText){
+  function SupportFlow(optionVal, optionText) {
+    const siteNum = Number($('#numSites').val());
+    const hasGP = $('input[name=gp]:checked').val();
+    const needsMockUp = $('input[name=supportMockup]:checked').val();
 
-    var getSiteNumber = Number($('#numSites').val());
-    var hasGP = $('input[name=gp]:checked').val();
+    let total = MenuPriceLookUp(optionVal);
+    let extraCost = 0;
 
-    var SupportTotalPrice = MenuPriceLookUp(MenuOptionVal);
+    $(".ProjName").text(optionText);
 
-    var needsMockUp = $('input[name=supportMockup]:checked').val();
-    var mockUpPrice = 500;
-
-    $(".ProjName").text(MenuOptionText);
-    $(".ProjCost").text("$" + SupportTotalPrice);
-
-
-
-    if(hasGP == "no_gp" && getSiteNumber != 1){
-
-      var costPerSite = getSiteNumber * 125;
-      costPerSite = costPerSite - 125; 
-
-      var siteNumPrice = getSiteNumber * 125;
-      SupportTotalPrice = MenuPriceLookUp(MenuOptionVal) + siteNumPrice ;
-
-      $(".NumSites").text(getSiteNumber);
-      $(".NumSitesCost").text("$" + costPerSite + " ($125/per additional site)");
-
-    }else{
-      $(".NumSites").text(getSiteNumber);
-      $(".NumSitesCost").text("0");
+    if (hasGP === "no_gp" && siteNum > 1) {
+      extraCost = (siteNum - 1) * 125;
+      total += siteNum * 125;
     }
 
-    if(needsMockUp == "yes_mu"){
-      SupportTotalPrice += mockUpPrice;
+    $(".NumSites").text(siteNum);
+    $(".NumSitesCost").text(extraCost ? `$${extraCost} ($125/per additional site)` : "0");
 
+    if (needsMockUp === "yes_mu") {
+      total += 500;
       $(".MockupName").text("Yes");
-      $(".MockupCost").text("$" + mockUpPrice);
-
-    }else{
+      $(".MockupCost").text("$500");
+    } else {
       $(".MockupName").text("No");
       $(".MockupCost").text("0");
-
     }
 
-    $(".supportPrice").text("$" + new Intl.NumberFormat('en-US').format(SupportTotalPrice));
-    $(".finalCost").text("$" + new Intl.NumberFormat('en-US').format(SupportTotalPrice));
-
-
+    const formattedPrice = new Intl.NumberFormat('en-US').format(total);
+    $(".ProjCost, .supportPrice, .finalCost").text(`$${formattedPrice}`);
   }
 
-  function MsFlow(buttonDirection){
-    if(buttonDirection == "next"){
-      var IsMsTemplate = $('input[name=isMS]:checked').val();
-      var isSupportTab = $(".is-support-proj").hasClass("active");
+  function MsFlow(direction) {
+    if (direction === "next") {
+      const isMs = $('input[name=isMS]:checked').val() === "is_ms";
+      const isSupportActive = $(".is-support-proj").hasClass("active");
 
       $(".active").removeClass("active");
 
-      if(IsMsTemplate == "is_ms"){
+      if (isMs) {
         $(".ms-description-tab").addClass("active");
-      }else{
-        if(isSupportTab){
-          $("#tmpBuild").prop( "checked", true );
-          MenuOptionVal = "tmpBuild";
-          MenuOptionText = "Custom Page Template ";
-
-          loadDeptFlow("is_support");
-        }else{
-          $(".is-support-proj").addClass("active");
-        }
-
+      } else if (isSupportActive) {
+        $("#tmpBuild").prop("checked", true);
+        MenuOptionVal = "tmpBuild";
+        MenuOptionText = "Custom Page Template ";
+        loadDeptFlow("is_support");
+      } else {
+        $(".is-support-proj").addClass("active");
       }
-    }else{
+    } else {
       $(".initial-flow-start").addClass("active");
     }
+
     showNextPrevBtn();
   }
 
   function prevslideLogic() {
+    const $active = $(".active");
+    const prevTab = $active.prev();
+    const isMsFlow = $active.parent().hasClass("ms-flow");
+    const isMs = $('input[name=isMS]:checked').val() === "is_ms";
 
-    var prevTab = $(".active").prev();
-    var isMsFlow = $(".active").parent().hasClass("ms-flow");
-    var IsMsTemplate = $('input[name=isMS]:checked').val();
+    if ($active.hasClass("initial-flow-start")) {
+      $active.removeClass("active");
 
-    if($(".active").hasClass("initial-flow-start")){
-
-      $(".active").removeClass("active");
-  
       $('#FormUpdate').load('flows/initial-flow.html', function() {
-        $(".active").removeClass("active");
-
         $(".custom-menu").addClass("active");
-
         showNextPrevBtn();
       });
-  
-    }else{
-      $(".active").removeClass("active");
-      if(isMsFlow && IsMsTemplate == "is_ms"){
+    } else {
+      $active.removeClass("active");
 
+      if (isMsFlow && isMs) {
         MsFlow("prev");
-
-      }else{
-
+      } else {
         prevTab.addClass("active");
-
       }
     }
-
-
   }
-  
-  function validateOption(){
-    var currentTabOptionChecked = $(".active input[type=radio]:checked").val();
-  
-    if($(".active").hasClass("support-number-sites")){
-  
-      var siteNumber= $(".active input[type=number]").val();
-      
-      if(siteNumber < 1){
-        if( !$(".active .mainQuestion").hasClass("invalid")){
-          $(".active .mainQuestion").append("<span class='invalid-message'>Please make a selection</span>");
-          $(".active .mainQuestion").addClass("invalid");
-          return false;
+
+  function validateOption() {
+    const $active = $(".active");
+    const $mainQuestion = $active.find(".mainQuestion");
+    const $invalidMsg = $mainQuestion.find(".invalid-message");
+
+    if ($active.hasClass("support-number-sites")) {
+      const siteNumber = Number($active.find("input[type=number]").val());
+
+      if (siteNumber < 1) {
+        if (!$mainQuestion.hasClass("invalid")) {
+          $mainQuestion.addClass("invalid").append("<span class='invalid-message'>Please make a selection</span>");
         }
-      }else{
-        $(".active .mainQuestion").removeClass("invalid");
-        $(".invalid-message").remove();
-        return true;
+        return false;
       }
-  
-    }else if(!currentTabOptionChecked){
-  
-      if( !$(".active .mainQuestion").hasClass("invalid")){
-        $(".active .mainQuestion").append("<span class='invalid-message'>Please make a selection</span>");
-        $(".active .mainQuestion").addClass("invalid");
-        
+    } else if (!$active.find("input[type=radio]:checked").length) {
+      if (!$mainQuestion.hasClass("invalid")) {
+        $mainQuestion.addClass("invalid").append("<span class='invalid-message'>Please make a selection</span>");
       }
-  
       return false;
-    }else{
-      $(".active .mainQuestion").removeClass("invalid");
-      $(".invalid-message").remove();
-      return true;
     }
-  
-  
-  }
-  
-  function MenuPriceLookUp(menuOption){
-  
-    const menuOptionList = [
-      ["tmpBuild",750],
-      ["dlrLocPage", 750],
-      ["homepageSupUpdate", 1000],
-      ["headerRedev", 625],
-      ["footerRedev", 250],
-      ["sqzPage", 500],
-      ["customNav", 500],
-      ["customPageBlock", 500]
-      ];
-  
-    for(var i = 0; i < menuOptionList.length; i++) {
-  
-      if(menuOptionList[i].includes(menuOption)){
-  
-        return menuOptionList[i][1];
-  
-      }
-  
-    }
-  
-  
+
+    $mainQuestion.removeClass("invalid");
+    $invalidMsg.remove();
+    return true;
   }
 
-  jQuery(document).ready(function(){
+  function MenuPriceLookUp(menuOption) {
+    const priceMap = {
+      tmpBuild: 750,
+      dlrLocPage: 750,
+      homepageSupUpdate: 1000,
+      headerRedev: 625,
+      footerRedev: 250,
+      sqzPage: 500,
+      customNav: 500,
+      customPageBlock: 500
+    };
+
+    return priceMap[menuOption] || 0;
+  }
+
+  $(document).ready(function() {
     $("#FormUpdate").load("flows/initial-flow.html");
-  
-  
-    $(document).on('click', '#prevBtn', function(){
-      prevslideLogic();
-    });
-  
-    $(document).on('click', '#nextBtn', function(){
-      if($(".active").hasClass("validate-form")){
-        if(validateOption()){
-          nextslideLogic();
-        }
-      }else{
+
+    $(document).on('click', '#prevBtn', prevslideLogic);
+
+    $(document).on('click', '#nextBtn', function() {
+      if ($(".active").hasClass("validate-form")) {
+        if (validateOption()) nextslideLogic();
+      } else {
         nextslideLogic();
       }
     });
   });
-  
-  
-  }(jQuery));
+
+})(jQuery);
